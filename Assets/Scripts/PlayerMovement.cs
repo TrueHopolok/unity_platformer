fa94c2f090456f477b98a0b22871a46b72890e87
TrueHopolok <vadim.truhanov@inbox.lv>
2026-09-14 22:32:14 +0300

    udpated player controller to include raycasts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,8 +1,14 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField] float checkSize = 0.02f;
+    [SerializeField] float raycastAmount = 5;
+    [SerializeField] bool useRaycastVariant = true;
+    [SerializeField] bool drawDebugRays = true;
     InputControls controls;
     InputAction jumpAction;
     InputAction moveAction;
@@ -37,8 +43,14 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        UpdateJumpStates_ColliderVariant();
-        // UpdateJumpStates_RaycastVariant();
+        if (useRaycastVariant)
+        {
+            UpdateJumpStates_RaycastVariant();
+        }
+        else
+        {
+            UpdateJumpStates_ColliderVariant();
+        }
         HandleVerticalMovement();
         HandleHorizontalMovement();
     }
@@ -46,7 +58,11 @@ public class PlayerMovement : MonoBehaviour
     // Can be extended to be used for wall jumping
     void UpdateJumpStates_ColliderVariant()
     {
-        const float checkSize = 0.02f;
+        if (!Mathf.Approximately(body.linearVelocityY, 0f))
+        {
+            isGrounded = false;
+            return;
+        }
 
         Vector2 hitboxPosition = transform.position;
         hitboxPosition += hitbox.offset;
@@ -55,12 +71,45 @@ public class PlayerMovement : MonoBehaviour
         // 0.01f needed to avoid border collission with player hitbox
         hitboxPosition.y -= hitboxSize.y * (0.5f + checkSize / 2) + 0.01f;
         hitboxSize.y = hitboxSize.y * checkSize;
-        isGrounded = Mathf.Approximately(body.linearVelocityY, 0f) && Physics2D.OverlapBox(hitboxPosition, hitboxSize, 0f) != null;
+        isGrounded = Physics2D.OverlapBox(hitboxPosition, hitboxSize, 0f) != null;
+
+        if (drawDebugRays)
+        {
+            hitboxPosition.y += checkSize / 2;
+            hitboxPosition.x -= hitboxSize.x / 2;
+            Debug.DrawRay(hitboxPosition, Vector2.down, Color.red);
+            hitboxPosition.x += hitboxSize.x;
+            Debug.DrawRay(hitboxPosition, Vector2.down, Color.red, 1f);
+        }
     }
 
     void UpdateJumpStates_RaycastVariant()
     {
-        // todo
+        if (!Mathf.Approximately(body.linearVelocityY, 0f))
+        {
+            isGrounded = false;
+            return;
+        }
+
+        Vector2 hitboxPosition = transform.position;
+        hitboxPosition += hitbox.offset;
+        Vector2 hitboxSize = hitbox.size;
+
+        // 0.01f needed to avoid border collission with player hitbox
+        hitboxPosition.y -= hitboxSize.y * 0.5f + 0.01f;
+        hitboxPosition.x -= hitboxSize.x / 2;
+
+        float delta = hitboxSize.x / (raycastAmount - 1);
+        for (int i = 0; i < raycastAmount; i++)
+        {
+            isGrounded = Physics2D.Raycast(hitboxPosition, Vector2.down, checkSize);
+            if (drawDebugRays)
+            {
+                Debug.DrawRay(hitboxPosition, Vector2.down, Color.red, 1f);
+            }
+            if (isGrounded) break;
+            hitboxPosition.x += delta;
+        }
     }
 
     void HandleVerticalMovement()
